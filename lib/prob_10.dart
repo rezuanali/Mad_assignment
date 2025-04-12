3f8d9cffbace1d3a4fefc7a0dd018be0104fe4fd
rezuanali <rasel15-5234@diu.edu.bd>
2025-04-13 00:34:00 +0600

    Bottom navigation with 3 tabs

---
+++ lib/prob_10.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Task10());
+
+class Task10 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: BottomNavExample());
+  }
+}
+
+class BottomNavExample extends StatefulWidget {
+  @override
+  _BottomNavExampleState createState() => _BottomNavExampleState();
+}
+
+class _BottomNavExampleState extends State<BottomNavExample> {
+  int currentIndex = 0;
+
+  final pages = [
+    Center(child: Text('Home Page')),
+    Center(child: Text('Search Page')),
+    Center(child: Text('Profile Page')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
